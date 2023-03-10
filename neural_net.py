e33207777c8bbec53f285ffd259cbd2b9974b50c
Henry C <henry.junkmail100@gmail.com>
2023-03-10 16:18:42 +0000

    defined nn class and written a function to produce yhat

--- neural_net.py
+++ neural_net.py
@@ -4,25 +4,29 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(120*120, 512),
+            nn.Linear(3*(120*120), 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.Linear(512, 2),
         )
 
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
-    
 
-model = NeuralNetwork().to(device)
-print(model)
+def run_neural_net(train_dataset, train_labels):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = NeuralNetwork().to(device)
+    X = train_dataset
+    logits = model(X)
+    pred_probab = nn.Softmax(dim=1)(logits)
+    y_pred = pred_probab.argmax(1)
+    print(f"Predicted label: {y_pred}")
+    print('actual label: ' + str(train_labels))
