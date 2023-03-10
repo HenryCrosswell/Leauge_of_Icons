5d2bcc2375e08a87ae9a37a20e9aeb61accfcdcb
Henry C <henry.junkmail100@gmail.com>
2023-03-10 11:13:19 +0000

    building a main script that pulls from other docs

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import numpy as np
+from loading import CustomDatasetFromCSV, csv_from_images_and_labels, split_dataset
+
+root_dir = 'F:/Users/Henry/Coding/League of Icons/Icons/'
+csv_path  = 'F:/Users/Henry/Coding/League of Icons/Icons/icon_labels.csv'
+image_dataset = CustomDatasetFromCSV(csv_path, root_dir)
+
+split_dataset(image_dataset)
